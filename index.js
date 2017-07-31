fa83ae806c996c87ee9379e755147131568fa975
Gupta <aayush123@gmail.com>
2017-07-31 15:42:34 +0530

    added two pages

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 let app = express();
+app.use(express.static('public'));
 
-app.get('/login', function(req, res) {
-  res.sendFile('./views/Login.html', {root: __dirname})
+app.get('/hht', function(req, res) {
+  res.sendFile('./views/HHT.html', {root: __dirname})
 });
+
+app.get('/se', function(req, res) {
+  res.sendFile('./views/SelfAssessment.html', {root: __dirname})
+});
+
 
 app.listen(3027)
 console.log("app started on 3027");
